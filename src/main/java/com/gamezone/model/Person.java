c89fed5508091e4a9a43279d5ef5f69becf1c0f5
ADRIANA DANGOND <apdangond@unicesar.edu.co>
2026-09-25 15:58:58 -0500

    feat(javadocs): implemet java docs in the class person

--- src/main/java/com/gamezone/model/Person.java
+++ src/main/java/com/gamezone/model/Person.java
@@ -1,16 +1,36 @@
 package com.gamezone.model;
 import java.io.Serializable;
 
+/**
+ * represents a generic person that interacts with the store.
+ * this class is abstract because a person must always be specialized
+ * as either a customer or a seller
+ */
+
 public abstract class Person  implements Serializable {
     private String name;
     private String identification;
     private String phoneNumber;
 
+    /**
+     * creates a new person with the given basic information
+     *
+     * @param name the person`s full name
+     * @param identification the person`s identification number
+     * @param phoneNumber the person`s contact phone number
+     *
+     */
     public Person(String name,String identification,String phoneNumber) {
         this.name = name;
         this.identification = identification ;
         this.phoneNumber = phoneNumber ;
     }
+
+    /**
+     * *returns the person`s name
+     *
+     * @return the name of the person
+     */
 
     public String getName() {
         return name;
@@ -35,6 +55,12 @@ public abstract class Person  implements Serializable {
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+    /**
+     * returns a description of the person, defined by each subclass
+     *
+     * @return a textual description specific to the person's role
+     */
 
     public abstract String getDescription();
 }
